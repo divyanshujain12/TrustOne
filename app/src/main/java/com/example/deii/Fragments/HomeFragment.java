24939490f1910a57dc8226c622c6ea673a71ad4c
divyanshu <divyanshujain12@hotmail.com>
2015-10-28 23:11:32 +0530

    no message

--- app/src/main/java/com/example/deii/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/deii/Fragments/HomeFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.andexert.library.RippleView;
 import com.example.deii.trustone.NavigationDrawerActivity;
@@ -15,11 +17,13 @@ import com.example.deii.trustone.R;
 /**
  * Created by Lenovo on 16-10-2015.
  */
-public class HomeFragment extends Fragment implements RippleView.OnRippleCompleteListener{
+public class HomeFragment extends Fragment implements RippleView.OnRippleCompleteListener, Animation.AnimationListener {
     private View view;
     private RippleView startHereRipple, horizonRipple, masterHealRipple, lockedRipple;
     private FragmentManager manager;
     private FragmentTransaction fragmentTransaction;
+    private Animation left_in1, right_in1, left_in2, right_in2;
+    boolean firstLeftIn = false, firstRightIn = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,32 +38,84 @@ public class HomeFragment extends Fragment implements RippleView.OnRippleComplet
     }
 
     private void InitViews() {
+
+        firstLeftIn = false;
+        firstRightIn = false;
         NavigationDrawerActivity.changeClassName("H O M E");
         startHereRipple = (RippleView) view.findViewById(R.id.startHereRipple);
         startHereRipple.setOnRippleCompleteListener(this);
 
         horizonRipple = (RippleView) view.findViewById(R.id.horizonRipple);
+        horizonRipple.setVisibility(View.GONE);
         horizonRipple.setOnRippleCompleteListener(this);
 
         masterHealRipple = (RippleView) view.findViewById(R.id.masterHealRipple);
+        masterHealRipple.setVisibility(View.GONE);
         masterHealRipple.setOnRippleCompleteListener(this);
 
         lockedRipple = (RippleView) view.findViewById(R.id.lockedRipple);
+        lockedRipple.setVisibility(View.GONE);
         lockedRipple.setOnRippleCompleteListener(this);
+
+        left_in1 = AnimationUtils.loadAnimation(getActivity(), R.anim.left_in);
+        left_in1.setAnimationListener(this);
+        right_in1 = AnimationUtils.loadAnimation(getActivity(), R.anim.right_in);
+        right_in1.setAnimationListener(this);
+
+        left_in2 = AnimationUtils.loadAnimation(getActivity(), R.anim.left_in);
+        left_in2.setAnimationListener(this);
+        right_in2 = AnimationUtils.loadAnimation(getActivity(), R.anim.right_in);
+        right_in2.setAnimationListener(this);
+
+        startHereRipple.startAnimation(left_in1);
+        /*horizonRipple.setAnimation(right_in);
+        masterHealRipple.setAnimation(left_in);
+        lockedRipple.setAnimation(right_in);*/
     }
 
     @Override
     public void onComplete(RippleView rippleView) {
-        updateFragment(1,"Sub Cat");
+        updateFragment(1, "Sub Cat");
     }
 
-    private void updateFragment(int categoryID,String name){
+    private void updateFragment(int categoryID, String name) {
         manager = getActivity().getSupportFragmentManager();
         fragmentTransaction = manager.beginTransaction();
 
-        SubCategoryFragment fragment = SubCategoryFragment.newInstance(categoryID,name);
-        fragmentTransaction.addToBackStack("fragment"+String.valueOf(categoryID));
+        SubCategoryFragment fragment = SubCategoryFragment.newInstance(categoryID, name);
+        fragmentTransaction.addToBackStack("fragment" + String.valueOf(categoryID));
         fragmentTransaction.replace(R.id.nav_contentframe, fragment);
         fragmentTransaction.commit();
     }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if (animation == left_in1) {
+            left_in1.cancel();
+            horizonRipple.setVisibility(View.VISIBLE);
+            horizonRipple.startAnimation(right_in1);
+        } else if (animation == right_in1) {
+            masterHealRipple.setVisibility(View.VISIBLE);
+            masterHealRipple.startAnimation(left_in2);
+        }
+        else if (animation == left_in2) {
+            left_in2.cancel();
+        lockedRipple.setVisibility(View.VISIBLE);
+            lockedRipple.startAnimation(right_in2);
+        }
+        /*else if (animation == right_in2) {
+
+        }*/
+    }
+
 }
