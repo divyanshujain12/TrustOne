131de86c51ab4b736690509c9c0e6c28af891224
divyanshu <divyanshujain12@hotmail.com>
2015-12-06 21:16:53 +0530

    no message

--- app/src/main/java/com/example/deii/ProductFragments/VideosFragment.java
+++ app/src/main/java/com/example/deii/ProductFragments/VideosFragment.java
@@ -6,12 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.deii.Utils.CallBackInterface;
 import com.example.deii.trustone.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by deii on 12/6/2015.
  */
-public class VideosFragment extends Fragment{
+public class VideosFragment extends Fragment implements CallBackInterface{
 
     public static VideosFragment newInstance(int pos, String name) {
         VideosFragment myFragment = new VideosFragment();
@@ -26,6 +30,23 @@ public class VideosFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.product_fragment, container, false);
+
+
+
+    }
+
+    @Override
+    public void onJsonObjectSuccess(JSONObject object) {
+
+    }
+
+    @Override
+    public void onJsonArrarSuccess(JSONArray array) {
+
+    }
+
+    @Override
+    public void onFailure(String str) {
 
     }
 }
